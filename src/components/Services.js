af34bf4268fbfc12a16b3f78e44ef2858b1ed012
Noah <nkwon.work@gmail.com>
2021-02-07 21:59:17 -0800

    Experience section created. Still need to fill in with information

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,7 +9,7 @@ const Services = () => {
 			<h1 className="py-5">my services</h1>
 			<div className="container">
 				<div className="row">
-					<div className="col-md-3 col-sm-6">
+					<div className="col-lg-3 col-md-6 col-sm-6">
 						<div className="box">
 							<div className="circle">
 								<FontAwesomeIcon className="icon" icon={faDesktop} size="2x" />
@@ -18,7 +18,7 @@ const Services = () => {
 							<p>I approach each project individually and always focus on the result.</p>
 						</div>
 					</div>
-					<div className="col-md-3 col-sm-6">
+					<div className="col-lg-3 col-md-6 col-sm-6">
 						<div className="box">
 							<div className="circle">
 								<FontAwesomeIcon className="icon" icon={faFileCode} size="2x" />
@@ -27,7 +27,7 @@ const Services = () => {
 							<p>Your website will be built with a new proven technology.</p>
 						</div>
 					</div>
-					<div className="col-md-3 col-sm-6">
+					<div className="col-lg-3 col-md-6 col-sm-6">
 						<div className="box">
 							<div className="circle">
 								<FontAwesomeIcon className="icon" icon={faFacebookF} size="2x" />
@@ -36,7 +36,7 @@ const Services = () => {
 							<p>Your potential clients will see your services or product on Facebook.</p>
 						</div>
 					</div>
-					<div className="col-md-3 col-sm-6">
+					<div className="col-lg-3 col-md-6 col-sm-6">
 						<div className="box">
 							<div className="circle">
 								<FontAwesomeIcon className="icon" icon={faGoogle} size="2x" />
